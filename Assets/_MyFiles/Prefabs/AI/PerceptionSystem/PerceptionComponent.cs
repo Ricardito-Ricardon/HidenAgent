1132de8b1beab6cf4bb406a0d972d56a7b4c6c65
Ricardito-Ricardon <rikielperro64@gmail.com>
2023-10-03 16:13:25 -0500

    Enemy AI

---
+++ Assets/_MyFiles/Prefabs/AI/PerceptionSystem/PerceptionComponent.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerceptionComponent : MonoBehaviour
+{
+    [SerializeField] Sense[] startSenses;
+
+    List<Sense> instantiatedSenses = new List<Sense>();
+    LinkedList<PerceptionStimuli> perceivedStimulis = new LinkedList<PerceptionStimuli>();
+
+    GameObject target;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        foreach (var sense in startSenses)
+        {
+            Sense newSense = ScriptableObject.Instantiate(sense);
+            instantiatedSenses.Add(newSense);
+            newSense.Init(this);
+            newSense.onPerceptionUpdated += PerceptionUpdated;
+        }
+    }
+
+    private void PerceptionUpdated(PerceptionStimuli stimuli, bool successfullySensed)
+    {
+        var node = perceivedStimulis.Find(stimuli);
+        if (successfullySensed)
+        {
+            if (node != null)
+            {
+                perceivedStimulis.AddAfter(node, stimuli);
+            }
+            else
+            {
+                perceivedStimulis.AddLast(stimuli);
+            }
+        }
+        else
+        {
+            perceivedStimulis.Remove(node);
+        }
+
+        if (perceivedStimulis.Count != 0)
+        {
+            if (target == null || target != perceivedStimulis.First.Value)
+            {
+                target = perceivedStimulis.First.Value.gameObject;
+            }
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var sense in instantiatedSenses)
+        {
+            sense.Update();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(Sense sense in instantiatedSenses)
+        {
+            sense.DrawDebug();
+        }
+
+        if(target != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(target.transform.position, 1f);
+            Gizmos.DrawLine(transform.position + Vector3.up, target.transform.position + Vector3.up);
+        }
+    }
+}
---
+++ Assets/_MyFiles/Prefabs/AI/PerceptionSystem/Sense.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class Sense : ScriptableObject
+{
+    public delegate void OnPerceptionUpdated(PerceptionStimuli stimuli, bool successfullySensed);
+    public event OnPerceptionUpdated onPerceptionUpdated;
+
+    [SerializeField] float forgetTime = 2f;
+    HashSet<PerceptionStimuli> currentlyPercievableStimuli = new HashSet<PerceptionStimuli>();
+    Dictionary<PerceptionStimuli, Coroutine> currentForgettingCoroutines = new Dictionary<PerceptionStimuli, Coroutine>();
+
+    public MonoBehaviour Owner
+    {
+        get;
+        private set;
+    }
+
+    public void Init(MonoBehaviour owner)
+    {
+        Owner = owner;
+    }
+
+    public void Update()
+    {
+        foreach(PerceptionStimuli stimuli in registeredStimulis)
+        {
+            if(IsStimuliSensable(stimuli) && !IsStimuliSensed(stimuli))
+            {
+                currentlyPercievableStimuli.Add(stimuli);
+                if(currentForgettingCoroutines.ContainsKey(stimuli))
+                {
+                    StopForgettingStimuli(stimuli);
+                }
+                else
+                {
+                    Debug.Log($"I sensed : {stimuli.gameObject.name}");
+                }
+            }
+
+            if(!IsStimuliSensable(stimuli) && IsStimuliSensed(stimuli))
+            {
+                currentlyPercievableStimuli.Remove(stimuli);
+                StartForgettingStimuli(stimuli);
+            }
+        }
+    }
+
+    private void StopForgettingStimuli(PerceptionStimuli stimuli)
+    {
+        Coroutine forgettingCoroutine = Owner.StartCoroutine(ForgettingCoroutine(stimuli));
+        currentForgettingCoroutines.Remove(stimuli);
+    }
+
+    private void StartForgettingStimuli(PerceptionStimuli stimuli)
+    {
+        Coroutine forgettingCoroutine = Owner.StartCoroutine(ForgettingCoroutine(stimuli));
+        currentForgettingCoroutines.Add(stimuli, forgettingCoroutine);
+    }
+
+    private IEnumerator ForgettingCoroutine(PerceptionStimuli stimuli)
+    {
+        yield return new WaitForSeconds(forgetTime);
+        currentForgettingCoroutines.Remove(stimuli); //we have forgot it already, coroutine is done.
+        onPerceptionUpdated.Invoke(stimuli, false);
+    }
+
+    private bool IsStimuliSensed(PerceptionStimuli stimuli)
+    {
+        return currentlyPercievableStimuli.Contains(stimuli);
+    }
+
+    public abstract bool IsStimuliSensable(PerceptionStimuli stimuli);
+
+    public virtual void DrawDebug()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    static HashSet<PerceptionStimuli> registeredStimulis = new HashSet<PerceptionStimuli>();
+    static public void RegisteredStimuli(PerceptionStimuli stimuli)
+    {
+        registeredStimulis.Add(stimuli);
+    }
+
+    static public void UnRegisterStimuli(PerceptionStimuli stimuli)
+    {
+        registeredStimulis.Remove(stimuli);
+    }
+}
